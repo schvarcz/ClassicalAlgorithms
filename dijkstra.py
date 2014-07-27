c29f1df5944a1dc7ef5b9f6821d9f94db655a142
Guilherme Schvarcz Franco <guilhermefrancosi@gmail.com>
2014-07-27 13:18:03 -0300

    Faz um exemplo mais bonitinho e inteligivel para o dijsktra

--- dijkstra.py
+++ dijkstra.py
@@ -13,11 +13,76 @@ def distancia(pt1,pt2):
     return sqrt(soma)
 
 #Gera lista de pontos em uma estrutura de lista de pontos(para plotagem), lista de arestas (para plotagem) e grafo num dicionário(para o dijkstra em si)
-pts = (randn(5,2)*5).tolist()
-pts += (randn(5,2)*5+asarray([10,10])).tolist()
-pts += (randn(5,2)*5+asarray([-10,10])).tolist()
-pts += (randn(5,2)*5+asarray([10,-10])).tolist()
-pts += (randn(5,2)*5+asarray([-10,-10])).tolist()
+pts = [
+        [-7.627792, -5.765306],
+        [-6.754342, 4.438776],
+        [-3.617866, 0.969388],
+        [-3.300248, -7.44898],
+        [-2.942928, -2.091837],
+        [-1.473945, 6.22449],
+        [-0.918114, -5.05102],
+        [-0.719603, 0.765306],
+        [-0.203474, 4.336735],
+        [0.947891, -1.581633],
+        [2.059553, 2.5],
+        [3.369727, 6.581633],
+        [4.124069, -3.72449],
+        [4.441687, 4.387755],
+        [4.878412, 1.122449],
+        [6.307692, -8.77551],
+        [6.665012, 5.459184],
+        [7.141439, -1.938776],
+        [8.054591, -6.326531]
+        ]
+
+arestas = [
+        [ pts[0], pts[1] ],
+        [ pts[0], pts[2] ],
+        [ pts[0], pts[3] ],
+        [ pts[0], pts[4] ],
+        [ pts[1], pts[2] ],
+        [ pts[1], pts[5] ],
+        [ pts[2], pts[4] ],
+        [ pts[2], pts[5] ],
+        [ pts[2], pts[7] ],
+        [ pts[2], pts[8] ],
+        [ pts[3], pts[4] ],
+        [ pts[3], pts[6] ],
+        [ pts[3], pts[15] ],
+        [ pts[4], pts[6] ],
+        [ pts[4], pts[7] ],
+        [ pts[5], pts[8] ],
+        [ pts[5], pts[11] ],
+        [ pts[6], pts[7] ],
+        [ pts[6], pts[9] ],
+        [ pts[6], pts[12] ],
+        [ pts[6], pts[15] ],
+        [ pts[7], pts[8] ],
+        [ pts[7], pts[9] ],
+        [ pts[7], pts[10] ],
+        [ pts[8], pts[10] ],
+        [ pts[8], pts[11] ],
+        [ pts[9], pts[10] ],
+        [ pts[9], pts[12] ],
+        [ pts[10], pts[11] ],
+        [ pts[10], pts[12] ],
+        [ pts[10], pts[13] ],
+        [ pts[10], pts[14] ],
+        [ pts[11], pts[13] ],
+        [ pts[11], pts[16] ],
+        [ pts[12], pts[14] ],
+        [ pts[12], pts[15] ],
+        [ pts[12], pts[17] ],
+        [ pts[12], pts[18] ],
+        [ pts[13], pts[14] ],
+        [ pts[13], pts[16] ],
+        [ pts[14], pts[16] ],
+        [ pts[14], pts[17] ],
+        [ pts[15], pts[18] ],
+        [ pts[16], pts[18] ],
+        [ pts[16], pts[17] ],
+        [ pts[17], pts[18] ],
+    ]
 
 dPts = {}
 for pt in pts:
@@ -29,16 +94,9 @@ for pt in pts:
             }
         })
 
-arestas = []
-for pt in pts:
-    for l in range(randint(2)+1):
-        sel = pt
-        while sel == pt:
-            sel = pts[randint(len(pts))]
-        if not ([pt,sel] in arestas or [sel,pt] in arestas):
-            arestas.append([pt,sel])
-            dPts[hashlist(pt)]["arestas"].append(sel)
-            dPts[hashlist(sel)]["arestas"].append(pt)
+for aresta in arestas:
+    dPts[hashlist(aresta[0])]["arestas"].append(aresta[1])
+    dPts[hashlist(aresta[1])]["arestas"].append(aresta[0])
 
 #Seleciona nó inicial e final, garantindo serem diferentes
 start = hashlist(pts[randint(len(pts))])
